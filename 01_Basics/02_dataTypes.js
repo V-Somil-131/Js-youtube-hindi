38136595ec121a6d92b0a645a1335df6b20af744
V-Somil-131 <161122273+V-Somil-131@users.noreply.github.com>
2024-04-10 12:06:15 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("Hello"); we are using node.js not browser
+
+console.log(3
+     + 
+     3); // code readability should be high 
+
+console.log("soumil");
+
+let name = "Soumil" ; 
+let age = 18 
+let isLoggedIn = false
+let state = null ;
+//******Primitive DataTypes :- *******/
+// number => 2 to the power 53 
+// bigInt
+// string => prefer in double quotes 
+// boolean => true/false
+// null => it is a standalone value in javascript 
+// undefined =>
+// symbol => unique 
+
+// *******Objects ***********
+console.log(typeof age)
+console.log(typeof null) // object 
+console.log(typeof undefined ) // undefined 
