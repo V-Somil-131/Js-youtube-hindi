13dd5bccb95140314e44e5dd0ffb288904e7c6ac
V-Somil-131 <161122273+V-Somil-131@users.noreply.github.com>
2024-04-10 10:46:53 +0000

    font size changed

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553 ;
+let accountEmail = "somil@google.com"
+var accountPassword = "12345" ;
+accoutCity = "Jaipur" ;
+// Variables names should be easily readable and understandable. 
